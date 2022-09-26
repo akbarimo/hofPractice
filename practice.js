5d0e93a62abac6928f91d0f67098d34115c799aa
akbarimo <mo.akbari15@gmail.com>
2022-09-25 17:53:49 -0700

    Completed hof practice

--- practice.js
+++ practice.js
@@ -1,17 +1,3 @@
-// This repo is optional extra practice to use the underscore functions.
-// Here we'll be writing new functions, but these functions will use
-// the underscore functions within them.
-
-// If you would like to take a look at the inputs that are passed into these functions, please
-// feel free to check out the data.js file.
-
-/*
- *
- *  _.each
- *
- */
-
-// use _.each to create a copy of the given array.
 var moreFruits = function (fruits) {
   var results = [];
 
@@ -22,135 +8,123 @@ var moreFruits = function (fruits) {
   return results;
 };
 
-// use _.each to traverse the number array and determine
-// which are multiples of five.
 var multiplesOfFive = function (numbers) {
-
-};
-
-// use _.each to build an array containing only tweets belonging to a specified user.
-var getUserTweets = function(tweets, user) {
-
-};
-
-/*
- *
- *  _.filter
- *
- */
-
-// use _.filter to return the fruits array with only the desired fruit.
-var onlyOneFruit = function (fruits, targetFruit) {
-
-};
-
-// use _.filter to return the fruits array with only fruits
-// starting with the letter 'P'.
-var startsWith = function (fruits, letter) {
-
-};
-
-// return a filtered array containing only cookie-type desserts.
-var cookiesOnly = function (desserts) {
-
-};
-
-// rebuild the getUserTweets function from above with _.filter instead
-var filterUserTweets = function(tweets, user) {
-
-};
-
-/*
- *
- *  _.map
- *
- */
-
-// given an array of strings, use _.map to return a new array containing all
-// strings converted to uppercase letters.
-var upperCaseFruits = function (fruits) {
-
-};
-
-// given an array of dessert objects, return a new array of objects
-// that have a new "glutenFree" property, with a boolean value.
-// TIP: Items that contain flour are not gluten-free.
-var glutenFree = function (desserts) {
-
-};
-
-// given an array of tweet objects, return a new array of strings
-// containing only the message properties.
-var allUserMessages = function(tweets) {
-
-};
-
-// use _.map to return an array of items with their sale prices, with a new property
-// containing the sale price. round any decimals to 2 places.
-//
-// having trouble with decimals? check out this article:
-// http://adripofjavascript.com/blog/drips/avoiding-problems-with-decimal-math-in-javascript.html
-//
-/*
-
- example output:
-  var salePrices = applyCoupon(groceries, 0.20);
-  [
-    {
-      id: 1,
-      product: 'Olive Oil',
-      price: '$12.1',
-      salePrice: '$9.68'
+  var multiples = 0;
+  _.each(numbers, function (numbers) {
+    if (numbers % 5 === 0) {
+      multiples++;
     }
-  ];
+  });
+  return multiples;
+};
 
-*/
+var getUserTweets = function(tweets, user) {
+  var result = [];
+  _.each(tweets, function (tweets, index) {
+    if (tweets.user === user) {
+      result.push(tweets);
+    }
+  });
+  return result;
+};
+
+var onlyOneFruit = function (fruits, targetFruit) {
+  return _.filter(fruits, function (fruits) {
+    return fruits === targetFruit;
+  });
+};
+
+var startsWith = function (fruits, letter) {
+  return _.filter(fruits, function (fruits) {
+    return fruits.startsWith(letter);
+  });
+};
+
+var cookiesOnly = function (desserts) {
+  return _.filter(desserts, function (desserts) {
+    return desserts.type === 'cookie';
+  });
+};
+
+var filterUserTweets = function(tweets, user) {
+  return _.filter(tweets, function (tweets) {
+    return tweets.user === user;
+  });
+};
+
+var upperCaseFruits = function (fruits) {
+  return _.map(fruits, function (fruits) {
+    return fruits.toUpperCase();
+  });
+};
+
+var glutenFree = function (desserts) {
+  return _.map(desserts, function (desserts) {
+    if (!desserts.ingredients.includes('flour')) {
+      desserts.glutenFree = true;
+      return desserts;
+    } else {
+      desserts.glutenFree = false;
+      return desserts;
+    }
+  });
+};
+
+var allUserMessages = function(tweets) {
+  return _.map(tweets, function (tweets) {
+    return tweets.message;
+  });
+};
+
 var applyCoupon = function (groceries, coupon) {
-
+  return _.map(groceries, function (groceries) {
+    groceries.salePrice = '$' + (groceries.price.slice(1) * (1 - coupon)).toPrecision(3);
+    return groceries;
+  });
 };
 
-/*
- *
- *  _.reduce
- *
- */
-
-// return the total price of all products.
 var sumTotal = function (products) {
-
+  return _.reduce(products, function (acc, item) {
+    return acc += Number(item.price.slice(1));
+  }, 0);
 };
 
-// return an object consisting of dessert types and how many of each.
-// exampleOutput: { dessertType: 3, dessertType2: 1 }
 var dessertCategories = function (desserts) {
-
+  return _.reduce(desserts, function (acc, item) {
+    if (!acc[item.type]) {
+      acc[item.type] = 1;
+    } else {
+      acc[item.type] += 1;
+    }
+    return acc;
+  }, {});
 };
 
-// return an object with the proper count of all user messages
-/*
- example output:
-  var tweetCountPerUser = countMessagesPerUser(tweets);
-  {
-    "douglascalhoun": 5,
-    "mracus": 6,
-    "shawndrost": 5,
-    "sharksforcheap": 3
-  }
-*/
 var countMessagesPerUser = function(tweets) {
-
+  return _.reduce(tweets, function (acc, item) {
+    if (!acc[item.user]) {
+      acc[item.user] = 1;
+    } else {
+      acc[item.user] += 1;
+    }
+    return acc;
+  }, {});
 };
 
-// given an array of movie data objects,return an array containing
-// movies that came out between 1990 and 2000.
-// TIP: use an array as your accumulator - don't push to an external array!
 var ninetiesKid = function (movies) {
-
+  return _.reduce(movies, function (acc, item) {
+    if (item.releaseYear >= 1990 && item.releaseYear <= 2000) {
+      acc.push(item.title);
+    }
+    return acc;
+  }, []);
 };
 
-// return an boolean stating if there exists a movie with a shorter
-// runtime than your time limit.
-// timeLimit is an integer representing a number of minutes.
 var movieNight = function (movies, timeLimit) {
-
+  return _.reduce(movies, function (acc, item) {
+    if (item.runtime <= timeLimit) {
+      return acc = true;
+    }
+    return acc;
+  }, false);
 };
